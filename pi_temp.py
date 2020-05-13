0b3368eddfac1574fc7c7246b9a693448c401caa
thenixonone <64987384+thenixonone@users.noreply.github.com>
2020-05-13 12:29:10 +0300

    Add files via upload

---
+++ pi_temp.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+
+from gpiozero import CPUTemperature
+import datetime
+import time
+
+# замер температуры
+def temp():
+    t = CPUTemperature()
+    return "Current temperature is: {}".format(t.temperature)
+
+# дата при записи в файл
+def date_time():
+    dateandtime = datetime.datetime.now()
+    date = dateandtime.strftime("%Y.%m.%d")
+    time = dateandtime.strftime("%H:%M:%S")
+    return "Date: {}; Time: {}".format(date, time)
+
+
+name = str(input("Enter file's name: "))
+def save(k, n):
+    with open(name, "a") as f:
+        f.write("{} -- {}\n".format(k, n))
+        return
+
+# вывод
+count = int(input("How many values need: "))
+count *= 10
+period = int(input("Enter period in sec: "))
+while count > 0:
+    save(temp(), date_time())
+    time.sleep(period)
+    count -= 10
+
+print("Finished!")
